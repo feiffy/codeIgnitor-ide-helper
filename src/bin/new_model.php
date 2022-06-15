c7faea4d9ae2ff2a1b44516ee467140a4a466ec7
FEIFFY.SONG <feifeifanye@hotmail.com>
2022-06-15 16:48:50 +0800

    add new_model.php
    add new_model.php

---
+++ src/bin/new_model.php
@@ -0,0 +1,41 @@
+<?php
+
+$app_path = dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))) . '/application';
+$app_model_path = $app_path . '/models';
+
+if (empty($_SERVER['argv'][1]) || empty($_SERVER['argv'][2])) {
+    exit('params db_name or model_name required!');
+}
+
+$db_name = $_SERVER['argv'][1];
+$model_name = $_SERVER['argv'][2];
+
+$model_classs_name = ucfirst($model_name);
+$table_name = strtolower($model_name);
+$db_name = strtolower($db_name);
+$created_at = date('Y-m-d');
+
+$new_model_tpl = <<<NEW_MODEL_TPL
+<?php
+
+/** 
+ * 
+ * @author 
+ * @created_at {$created_at}
+ */
+class {$model_classs_name}_model extends Che300_Model
+{
+    const TABLE_NAME = '{$table_name}';
+    public function __construct()
+    {
+        parent::__construct(self::TABLE_NAME, 'id', '{$db_name}');
+    }
+
+}
+NEW_MODEL_TPL;
+
+$app_model_path = $app_model_path . '/' . $db_name . '/' . $model_classs_name . '_model.php';
+file_put_contents($app_model_path, $new_model_tpl);
+
+// regenerate my_models
+require_once 'make.php';
